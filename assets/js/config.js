a4713ffd567c32b05cc0d84eff6e19373678dfb9
Nayeem Limon <workplace.nayeemislam@gmail.com>
2026-05-18 21:17:00 +0600

    .

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 // Configuration file for API endpoints and environment settings.
-export const API_BASE_URL = "https://YOUR_SCRIPT_ID_HERE/exec";
+export const API_BASE_URL = "https://script.google.com/macros/s/AKfycbyhRzkW0z9vf3JDHVwwa3UPUvgto5rLfcOejSPm0m1W4lI9X9fQrk_PKxLazwE5BgUF/exec";
 
 // Change the value above after you deploy the Google Apps Script backend.

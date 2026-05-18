bca7dccf7f2bc76203344b2461e3398157a68cdc
Nayeem Limon <workplace.nayeemislam@gmail.com>
2026-05-18 22:32:37 +0600

    applied url

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 // Configuration file for API endpoints and environment settings.
- const API_BASE_URL = "https://script.google.com/macros/s/AKfycbyoPGxr3kLKflDBsk_FESm7qqJ5PWXjhX5WK4aUFaeWVG7CDY6yR33pnhDI_7-izqvr/exec";
+ const API_BASE_URL = "https://script.google.com/macros/s/AKfycbxmy7NYroohanocebMv-2HZJ-fL9Ko7xqa-yyT_I7vpkEZFUHmL1EcbMnaMuHsY3cMx/exec";
 
 // Change the value above after you deploy the Google Apps Script backend.
